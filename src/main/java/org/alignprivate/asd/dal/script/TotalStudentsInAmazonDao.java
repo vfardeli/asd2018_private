fa42a59e37d6a63971a07288dac45e9052b06286
yang <yang@yangdeMacBook-Pro.local>
2018-02-22 14:01:58 -0800

    Add scripts to extract data from private to public

---
+++ src/main/java/org/alignprivate/asd/dal/script/TotalStudentsInAmazonDao.java
@@ -0,0 +1,77 @@
+package org.alignprivate.asd.dal.script;
+
+import java.sql.SQLException;
+
+public class TotalStudentsInAmazonDao {
+    // Singleton pattern
+    private static TotalStudentsInAmazonDao instance = null;
+
+    /**
+     * Default Constructor.
+     */
+    public TotalStudentsInAmazonDao() { }
+
+    /**
+     * Singleton Pattern.
+     *
+     * @return Total Students Who Worked In Amazon Dao.
+     * @throws SQLException when connection to the DB has something wrong.
+     */
+    public static TotalStudentsInAmazonDao getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new TotalStudentsInAmazonDao();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Get total number of students who worked in Amazon
+     * from private database.
+     *
+     * @return Total number of students who worked in Amazon
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public int getTotalStudentsFromPrivateDB() throws SQLException {
+        String query = "SELECT COUNT(*) AS TOTAL_AMAZON_STUDENT FROM ( " +
+                "SELECT S.NeuId, COUNT(*) " +
+                "FROM AlignPrivate.Students AS S " +
+                "INNER JOIN AlignPrivate.WorkExperiences AS E ON S.NeuId = E.NeuId " +
+                "INNER JOIN AlignPrivate.Companies AS C ON E.CompanyId = C.CompanyId " +
+                "WHERE C.CompanyName = \"Amazon\" " +
+                "GROUP BY S.NeuId) AS Student_Amazon;";
+
+        PrivateDatabaseEtlQuery privateDatabaseEtlQuery = new PrivateDatabaseEtlQuery();
+        return privateDatabaseEtlQuery.getSingleValueQuery(
+                query,
+                "TOTAL_AMAZON_STUDENT");
+    }
+
+    /**
+     * Update the number of students who worked in Amazon in public database.
+     *
+     * @param totalStudentsInAmazon
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public void updateTotalStudentsInAmazon(int totalStudentsInAmazon) throws SQLException {
+        String updateStudentsInAmazon =
+                "UPDATE SingleValueAggregatedData SET DataValue = ? WHERE " +
+                        "DataKey = \"TotalStudentsWhoWorkInAmazon\";";
+        PublicDatabaseEtlQuery publicPublicDatabaseEtlQuery = new PublicDatabaseEtlQuery();
+        publicPublicDatabaseEtlQuery.updateSingleValueQuery(updateStudentsInAmazon, totalStudentsInAmazon);
+    }
+
+    /**
+     * Get the number of students who worked in Amazon from public database.
+     *
+     * @return the number of students who worked in Amazon.
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public int getTotalStudentsInAmazon() throws SQLException {
+        String getStudentsInAmazon =
+                "SELECT DataValue FROM SingleValueAggregatedData " +
+                        "WHERE DataKey = \"TotalStudentsWhoWorkInAmazon\";";
+        PublicDatabaseEtlQuery publicPublicDatabaseEtlQuery = new PublicDatabaseEtlQuery();
+        return publicPublicDatabaseEtlQuery.getSingleValueQuery(getStudentsInAmazon);
+    }
+}
---
+++ src/main/java/org/alignprivate/asd/dal/script/TotalStudentsInGoogleDao.java
@@ -0,0 +1,78 @@
+package org.alignprivate.asd.dal.script;
+
+import java.sql.SQLException;
+
+public class TotalStudentsInGoogleDao {
+    // Singleton pattern
+    private static TotalStudentsInGoogleDao instance = null;
+
+    /**
+     * Default Constructor.
+     */
+    public TotalStudentsInGoogleDao() { }
+
+    /**
+     * Singleton Pattern.
+     *
+     * @return Total Students Who Worked In Google Dao.
+     * @throws SQLException when connection to the DB has something wrong.
+     */
+    public static TotalStudentsInGoogleDao getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new TotalStudentsInGoogleDao();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Get total number of students who worked in Google
+     * from private database.
+     *
+     * @return Total number of students who worked in Google
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public int getTotalStudentsFromPrivateDB() throws SQLException {
+        String query = "SELECT COUNT(*) AS TOTAL_GOOGLE_STUDENT FROM ( " +
+                "SELECT S.NeuId, COUNT(*) " +
+                "FROM AlignPrivate.Students AS S " +
+                "INNER JOIN AlignPrivate.WorkExperiences AS E ON S.NeuId = E.NeuId " +
+                "INNER JOIN AlignPrivate.Companies AS C ON E.CompanyId = C.CompanyId " +
+                "WHERE C.CompanyName = \"Google\" " +
+                "GROUP BY S.NeuId) AS Student_Google;";
+
+        PrivateDatabaseEtlQuery privateDatabaseEtlQuery = new PrivateDatabaseEtlQuery();
+        return privateDatabaseEtlQuery.getSingleValueQuery(
+                query,
+                "TOTAL_GOOGLE_STUDENT");
+    }
+
+    /**
+     * Update the number of students who worked in Google in public database.
+     *
+     * @param totalStudentsInGoogle
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public void updateTotalStudentsInGoogle(int totalStudentsInGoogle) throws SQLException {
+        String updateStudentsInGoogle =
+                "UPDATE SingleValueAggregatedData SET DataValue = ? WHERE " +
+                        "DataKey = \"TotalStudentsWhoWorkInGoogle\";";
+        PublicDatabaseEtlQuery publicPublicDatabaseEtlQuery = new PublicDatabaseEtlQuery();
+        publicPublicDatabaseEtlQuery.updateSingleValueQuery(updateStudentsInGoogle, totalStudentsInGoogle);
+    }
+
+    /**
+     * Get the number of students who worked in Google from public database.
+     *
+     * @return the number of students who worked in Google.
+     * @throws SQLException when connection to the DB has something wrong
+     */
+    public int getTotalStudentsInGoogle() throws SQLException {
+        String getStudentsInGoogle =
+                "SELECT DataValue FROM SingleValueAggregatedData " +
+                        "WHERE DataKey = \"TotalStudentsWhoWorkInGoogle\";";
+        PublicDatabaseEtlQuery publicPublicDatabaseEtlQuery = new PublicDatabaseEtlQuery();
+        return publicPublicDatabaseEtlQuery.getSingleValueQuery(getStudentsInGoogle);
+    }
+}
+
